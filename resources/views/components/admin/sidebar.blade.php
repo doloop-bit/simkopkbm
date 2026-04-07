6c6196de4570830704a481938be8ce0e70d8041e
doloop-bit <latip.thok@gmail.com>
2026-04-07 13:59:31 +0700

    feat: restrict sidebar menu visibility based on user roles

--- resources/views/components/admin/sidebar.blade.php
+++ resources/views/components/admin/sidebar.blade.php
@@ -16,31 +16,33 @@
 <x-ui.menu activate-by-route>
     <x-ui.menu-item title="Dashboard" icon="o-home" :link="route('dashboard')" />
 
-    <x-ui.menu-sub title="Data Master" icon="o-book-open" :active="request()->routeIs('students.*') || request()->routeIs('ptk.*') || request()->routeIs('users.*') || request()->routeIs('admin.registrations.*')">
-        <x-ui.menu-item title="Siswa" icon="o-users" :link="route('students.index')" />
-        <x-ui.menu-item title="Penempatan Kelas" icon="o-arrows-right-left" :link="route('students.class-placement')" />
-        <x-ui.menu-item title="PTK" icon="o-briefcase" :link="route('ptk.index')" />
-        <x-ui.menu-item title="Pengguna" icon="o-user-circle" :link="route('users.index')" />
-        <x-ui.menu-item title="Pendaftaran" icon="o-user-plus" :link="route('admin.registrations.index')" />
-    </x-ui.menu-sub>
+    @if(auth()->user()->isAdmin() || auth()->user()->isHeadmaster() || auth()->user()->isYayasan())
+        <x-ui.menu-sub title="Data Master" icon="o-book-open" :active="request()->routeIs('students.*') || request()->routeIs('ptk.*') || request()->routeIs('users.*') || request()->routeIs('admin.registrations.*')">
+            <x-ui.menu-item title="Siswa" icon="o-users" :link="route('students.index')" />
+            <x-ui.menu-item title="Penempatan Kelas" icon="o-arrows-right-left" :link="route('students.class-placement')" />
+            <x-ui.menu-item title="PTK" icon="o-briefcase" :link="route('ptk.index')" />
+            <x-ui.menu-item title="Pengguna" icon="o-user-circle" :link="route('users.index')" />
+            <x-ui.menu-item title="Pendaftaran" icon="o-user-plus" :link="route('admin.registrations.index')" />
+        </x-ui.menu-sub>
 
-    <x-ui.menu-sub title="Akademik" icon="o-academic-cap" :active="request()->routeIs('academic.*')">
-        <x-ui.menu-item title="Tahun Ajaran" icon="o-calendar" :link="route('academic.years')" />
-        <x-ui.menu-item title="Jenjang" icon="o-academic-cap" :link="route('academic.levels')" />
-        <x-ui.menu-item title="Kelas" icon="o-building-office" :link="route('academic.classrooms')" />
+        <x-ui.menu-sub title="Akademik" icon="o-academic-cap" :active="request()->routeIs('academic.*')">
+            <x-ui.menu-item title="Tahun Ajaran" icon="o-calendar" :link="route('academic.years')" />
+            <x-ui.menu-item title="Jenjang" icon="o-academic-cap" :link="route('academic.levels')" />
+            <x-ui.menu-item title="Kelas" icon="o-building-office" :link="route('academic.classrooms')" />
 
-        <x-ui.menu-item title="Mata Pelajaran" icon="o-book-open" :link="route('academic.subjects')" />
-        <x-ui.menu-item title="Penugasan Guru" icon="o-user-group" :link="route('academic.assignments')" />
-        <x-ui.menu-item title="Presensi" icon="o-check-badge" :link="route('academic.attendance')" />
-        <x-ui.menu-item title="Ekstrakurikuler" icon="o-trophy" :link="route('academic.extracurriculars')" />
-    </x-ui.menu-sub>
+            <x-ui.menu-item title="Mata Pelajaran" icon="o-book-open" :link="route('academic.subjects')" />
+            <x-ui.menu-item title="Penugasan Guru" icon="o-user-group" :link="route('academic.assignments')" />
+            <x-ui.menu-item title="Presensi" icon="o-check-badge" :link="route('academic.attendance')" />
+            <x-ui.menu-item title="Ekstrakurikuler" icon="o-trophy" :link="route('academic.extracurriculars')" />
+        </x-ui.menu-sub>
 
-    <x-ui.menu-sub title="Penilaian & Raport" icon="o-pencil-square" :active="request()->routeIs('admin.assessments.*') || request()->routeIs('admin.report-card.*')">
-        <x-ui.menu-item title="Penilaian PAUD" icon="o-clipboard-document-check" :link="route('admin.assessments.competency')" />
-        <x-ui.menu-item title="Raport Kesetaraan" icon="o-document-chart-bar" :link="route('admin.report-card.grading')" />
-    </x-ui.menu-sub>
+        <x-ui.menu-sub title="Penilaian & Raport" icon="o-pencil-square" :active="request()->routeIs('admin.assessments.*') || request()->routeIs('admin.report-card.*')">
+            <x-ui.menu-item title="Penilaian PAUD" icon="o-clipboard-document-check" :link="route('admin.assessments.competency')" />
+            <x-ui.menu-item title="Raport Kesetaraan" icon="o-document-chart-bar" :link="route('admin.report-card.grading')" />
+        </x-ui.menu-sub>
+    @endif
 
-    @if(auth()->user()->isAdmin() || auth()->user()->isTreasurer())
+    @if(auth()->user()->isAdmin() || auth()->user()->isTreasurer() || auth()->user()->isHeadmaster() || auth()->user()->isYayasan())
         <x-ui.menu-sub title="Keuangan" icon="o-banknotes" :active="request()->routeIs('financial.*')">
             <x-ui.menu-item title="Transaksi Keuangan" icon="o-wallet" :link="route('financial.transactions')" />
             <x-ui.menu-item title="RAB / Anggaran" icon="o-document-currency-dollar" :link="route('financial.budget-plans')" />
