a2d132ef179d9734b8c16dd7cbc5a2417c96ef0f
doloop bit <latip.thok@gmail.com>
2026-04-12 19:05:01 +0700

    chore: remove unused legacy MPA shell and landing page files

--- resources/views/paud.blade.php
+++
@@ -1,8 +0,0 @@
-<x-layouts.spa-shell 
-    :title="($program->name ?? 'PAUD Ceria') . ' - Belajar & Bermain'"
-    :program-name="$program->name ?? 'PAUD Ceria'"
-    :program-logo="$program->image_path ? Storage::url($program->image_path) : null"
-    entry-point="resources/js/paud.js"
->
-    <div id="paud-app"></div>
-</x-layouts.spa-shell>
